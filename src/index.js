37d08e97c2198010c3cfb1f21dcc823923a1ddf7
cassio8186 <cassio8186@gmail.com>
2019-12-16 16:58:06 -0300

    aula 17 desafio produto
    Signed-off-by: cassio8186 <cassio8186@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,21 +1,64 @@
 const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
+	scalar Date
+	type Produto {
+		nome: String!
+		preco: Float!
+		desconto: Float
+		precoComDesconto: Float!
+	}
+	type Usuario {
+		id: ID
+		nome: String!
+		email: String!
+		idade: Int!
+		salario: Float
+		vip: Boolean
+	}
 	# Pontos de entrada da sua API
 	type Query {
 		ola: String!
-		dataCerta: String!
+		dataCerta: Date!
+		usuarioLogado: Usuario
+		produtoEmDestaque: Produto
 	}
 `;
 
 const resolvers = {
+	Produto: {
+		precoComDesconto(produto) {
+			if (produto.desconto) {
+				return produto.preco * (1 - produto.desconto);
+			} else {
+				return produto.preco;
+			}
+		}
+	},
+	Usuario: {
+		salario(usuario) {
+			return usuario.salario_real * 15;
+		}
+	},
 	Query: {
+		produtoEmDestaque() {
+			return { nome: "Cassio", preco: 24.41, desconto: 0.85 };
+		},
 		ola() {
 			return "Olá, tudo bem?";
 		},
 		dataCerta() {
-			const date = new Date();
-			return date.toTimeString();
+			return new Date();
+		},
+		usuarioLogado() {
+			return {
+				id: 1,
+				nome: "ana",
+				email: "test@gmail.com",
+				idade: 21,
+				salario_real: 124.321,
+				vip: true
+			};
 		}
 	}
 };
