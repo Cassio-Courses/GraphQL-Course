1b1caab92df18abe5e23f1be7b06076ae7ee2cf7
cassio8186 <cassio8186@gmail.com>
2019-12-17 13:00:32 -0300

    aula 19 retornando array
    Signed-off-by: cassio8186 <cassio8186@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 const { ApolloServer, gql } = require("apollo-server");
-
+const usuarios = [
+	{
+		id: 1,
+		nome: "Cassio",
+		email: "cassio8186@gmail.com",
+		idade: 21,
+		salario: 1200.24,
+		vip: true
+	},
+	{
+		id: 2,
+		nome: "Daniele",
+		email: "daniele@gmail.com",
+		idade: 25,
+		salario: 1400.24,
+		vip: false
+	},
+	{
+		id: 3,
+		nome: "Junin",
+		email: "junin@gmail.com",
+		idade: 21,
+		salario: 19403.24,
+		vip: false
+	}
+];
 const typeDefs = gql`
 	scalar Date
+
 	type Produto {
 		nome: String!
 		preco: Float!
@@ -22,9 +48,10 @@ const typeDefs = gql`
 		dataCerta: Date!
 		usuarioLogado: Usuario
 		produtoEmDestaque: Produto
+		numerosMegaSena: [Int]
+		usuarios: [Usuario]!
 	}
 `;
-
 const resolvers = {
 	Produto: {
 		precoComDesconto(produto) {
@@ -41,6 +68,9 @@ const resolvers = {
 		}
 	},
 	Query: {
+		usuarios() {
+			return usuarios;
+		},
 		produtoEmDestaque() {
 			return { nome: "Cassio", preco: 24.41, desconto: 0.85 };
 		},
@@ -59,6 +89,12 @@ const resolvers = {
 				salario_real: 124.321,
 				vip: true
 			};
+		},
+		numerosMegaSena() {
+			const crescente = (a, b) => a - b;
+			return Array(6)
+				.fill(0)
+				.map(() => parseInt((Math.random() * 60).sort(crescente)));
 		}
 	}
 };
